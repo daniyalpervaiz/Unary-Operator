4151c49a213aaf4495a0a73c713886490c17a8a6
Daniyal Pervaiz <160670546+daniyalpervaiz@users.noreply.github.com>
2024-03-08 01:39:47 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+//Post incriment
+var num = 0;
+console.log(num); //0
+console.log(num++); //0
+console.log(num); //1
+console.log(num++); //1
+console.log(num); //2
+console.log(num++); //2
+console.log(num); //3
+console.log(num = num + 5); //8
+//pre increment
+var a = 0;
+console.log(++a); //1
+console.log(a); //1
+console.log(++a); //2
+console.log(a); //2
+console.log(++a); //3
+console.log(a = a + 5); //8
+//post Decrement
+var b = 15;
+console.log(b); //15
+console.log(b--); //15
+console.log(b); //14
+console.log(b--); //14
+console.log(b); //13
+console.log(b = b - 5); //8
+//pre Decrement
+var c = 15;
+console.log(--c); //14
+console.log(c); //14
+console.log(--c); //13
+console.log(c); //13
+console.log(c = c - 5); //8
---
+++ main.ts
@@ -0,0 +1,38 @@
+//UNARY OPERATOR
+
+//post Incriment
+let num = 0
+console.log(num);//0
+console.log(num++);//0
+console.log(num);//1
+console.log(num++);//1
+console.log(num);//2
+console.log(num++);//2
+console.log(num);//3
+console.log(num=num+5);//8
+
+//pre Increment
+let a=0
+console.log(++a);//1
+console.log(a);//1
+console.log(++a);//2
+console.log(a);//2
+console.log(++a);//3
+console.log(a=a+5);//8
+
+//post Decrement
+let b=15
+console.log(b);//15
+console.log(b--);//15
+console.log(b);//14
+console.log(b--);//14
+console.log(b);//13
+console.log(b=b-5);//8
+
+//pre Decrement
+let c=15
+console.log(--c);//14
+console.log(c);//14
+console.log(--c);//13
+console.log(c);//13
+console.log(c=c-5);//8
